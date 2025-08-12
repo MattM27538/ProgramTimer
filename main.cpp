d969cc17ea08ebba402baef0452b5e0cba08c654
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-08-12 16:04:32 -0700

    move main() to main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "timer.h"
+#include "programTimer.h"
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]){
+    if(checkForCMDArguments(argc) == false){
+        std::cerr << "Incorrect number of inputs. Expected 2, found " << argc <<" Exiting program timer.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    const char* executable{argv[1]};
+    
+    constexpr auto numOfExecutableCalls{10};
+    
+    double totalRunTime{0.0};
+    
+    Timer programTimer{};
+
+    callExternalExecutable(executable, programTimer, numOfExecutableCalls, totalRunTime);
+
+    std::cout << "Program Completed. Total time elapsed: " << totalRunTime <<"\n";
+    std::cout << "Average time elapsed for program: " << totalRunTime/numOfExecutableCalls <<"\n";
+
+    return EXIT_SUCCESS;
+}
